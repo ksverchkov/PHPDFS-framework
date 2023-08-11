b0ed1c308ae68f9bb3bf8f6bebfeb04d4a5e5c27
Kirill Sverchkov <107706691+ksverchkov@users.noreply.github.com>
2023-08-11 15:17:28 +0000

    Updated classes

--- src/PHPDFS/Router/Router.php
+++ src/PHPDFS/Router/Router.php
@@ -35,16 +35,57 @@ class Router implements RouterInterface
         $this->routes[$route] = $params;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function dispatch(string $url) : void
     {
+        if ($this->match($url)){
+            $controllerString = $this->params["controller"];
+            $controllerString = $this->transformUpperCamelCase($controllerString);
+            $controllerString = $this->getNamespace($controllerString);
+        }
+    }
 
+    public function transformUpperCamelCase(string $string)
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
     }
 
     /**
-     * m
+     * Match the route to the routes in the routing table, setting the $this->params property
+     * if a route is found 
+     * 
+     * @param string $url
+     * @return bool
      */
     private function match(string $url): bool
     {
-        
+        foreach ($this->routes as $route => $params){
+            if (preg_match($route, $url, $matches)){
+                foreach ($matches as $key => $param){
+                    if (is_string($key)){
+                        $params[$key] = $param;
+                    }
+                }
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get the namespace for the controller class, the name space defined within the route parameters
+     * @param string $string
+     * @return string
+     */
+    public function getNamespace(string $string) : string
+    {
+        $namespace = 'App\Controller\\';
+        if (array_key_exists('namespace', $this->params)){
+            $namespace .= $this->params["namespace"] . '\\';
+        }
+        return $namespace;
     }
 }
