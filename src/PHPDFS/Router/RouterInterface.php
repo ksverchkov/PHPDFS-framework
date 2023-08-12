620f51304c86585e1b523306a0782e4dba74f40e
Kirill Sverchkov <107706691+ksverchkov@users.noreply.github.com>
2023-08-12 06:20:58 +0000

    Added router class

--- src/PHPDFS/Router/RouterInterface.php
+++ src/PHPDFS/Router/RouterInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace PHPDFS\Router;
 
@@ -8,20 +8,20 @@ interface RouterInterface
 {
 
     /**
-     * Add a route to the routing table
+     * Simple add a route to the routing table
      * 
      * @param string $route
      * @param array $params
      * @return void
      */
-    public function add(string $route, array $params) : void;
+    public function add(string $route, array $params = []) : void;
 
     /**
-     * Dispath route and create controller objects and execute the default method
+     * Dispatch route and create controller objects and execute the default method 
      * on that controller object
      * 
      * @param string $url
-     * @return void 
+     * @return void
      */
     public function dispatch(string $url) : void;
 
